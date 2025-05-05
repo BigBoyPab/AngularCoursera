ab3b8ee1000b64bee57933edabd1d1fcbba543e1
Pab0 <saipavansavaram@Pab0s-MacBook-Pro.local>
2025-05-05 17:17:19 -0400

    name value calculator

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-( function (){
+( function () {
   'use strict';
-  angular.module('MyFirstApp'),[])
-  .controller("MyFirstController",function($name1){
-    $name1.name="Pabo";
+  angular.module("NameCalculator",[])
+  .controller("CalculateTextValue", function($scope){
+  $scope.InpText="";
+  $scope.textValue=0;
+
+  $scope.outValue=function() {
+    var sum=calculate($scope.InpText);
+    $scope.textValue=sum;
+  }
+  function calculate(string)
+  {
+    var sum1=0;
+    for(var i=0;i<string.length;i++)
+    {
+      sum1+=string.charCodeAt(i);
+    }
+    return sum1;
+  }
 
   })
-});();
+}
+
+)();
