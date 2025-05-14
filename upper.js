aaaf4e771d106d084dbb2ed289431a26d9d3f82b
Pab0 <saipavansavaram@Pab0s-MacBook-Pro.local>
2025-05-14 14:55:59 -0400

    added images

--- upper.js
+++ upper.js
@@ -1,11 +1,13 @@
 (function() {
   angular.module("UpperCaseConverter",[])
-  .controller("toUpper", function ($scope,$filter){
+  .controller("toUpper", UpperC)
+  toUpper.$inject=["$scope","$filter"]
+function UpperC ($scope,$filter){
 $scope.input="pabo";
 $scope.up=function() {
   var s=$filter('uppercase');
   $scope.input=s($scope.input);
 }
 }
-)
+
 })();
